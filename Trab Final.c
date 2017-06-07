2b6cd2db764d2c83ecd864a34f4c8b4ae33a7b26
IllucidMan <pasquamat@gmail.com>
2017-06-07 11:25:40 -0300

    Add files via upload

---
+++ Trab Final.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct{
+ char heroi[50];
+ int poder;
+} heroes;
+
+void simularBatalha(heroes * herois[], heroes *thanos){
+
+}
+
+void imprime(heroes * herois[], heroes * thanos){
+
+heroes** selecaoParaAtaque(heroes vetor[])
+}
+void leHeroi(heroes * t)  {
+    int i=0    
+    FILE * entrada  
+    entrada = fopen ("herois.txt", "r")      
+    if (entrada != NULL){
+        fscanf (entrada,"%s",thanos->heroi);
+        fscanf (entrada,"%d",thanos->poder);
+        do{
+            fscanf(entrada,"%s",herois[i]->heroi[50]);
+            fscanf(entrada,"%d",herois[i]->poder);
+            i++
+
+        }while(!feof(saida));
+
+
+    } else { }// treta no file 
+                
+           
+    
+        
+
+
+
+}
+
+
+int main(void){
+heroes thanos;
+leHeroi(&thanos-> heroi);
+leHeroi(&thanos-> poder);
+
+heroes herois[11];
+for (i=0; i < 11; i++){
+    leHeroi(&herois[i]->heroi);
+    leHeroi(&herois[i]->poder);
+    }
+return 0;
+}
+
+
